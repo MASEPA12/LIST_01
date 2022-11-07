d11328c6ad8e408a57701bdb551e8ce6eccf942a
MASEPA12 <114388814+MASEPA12@users.noreply.github.com>
2022-11-07 18:25:20 +0100

    exercise_1_done

--- Assets/ONE.cs
+++ Assets/ONE.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class ONE : MonoBehaviour
 {
+    //VARIABLE DECLARATION
+    public float variable1 = 5;
+    public float variable2 = 3;
+    public float variable3 = 9;
+    public float result;
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        result = variable1 + variable2 + variable3;
+        Debug.Log($"The sum of {variable1}, {variable2} and {variable3} is {result}");
     }
 }
