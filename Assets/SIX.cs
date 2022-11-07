2533cff20d33e1f9f78aa5fdda0aef778d92b35c
MASEPA12 <114388814+MASEPA12@users.noreply.github.com>
2022-11-07 19:36:49 +0100

    Exercise_6_done

--- Assets/SIX.cs
+++ Assets/SIX.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class SIX : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // VARIABLE DECLARATION
+    public int dayOfTheWeek;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Start()
+    {       //if the number of the day is 1,2,3,4,5 it doesn't belongs to the weekend
+        if(dayOfTheWeek == 1 || dayOfTheWeek == 2 || dayOfTheWeek == 3 || dayOfTheWeek == 4 || dayOfTheWeek == 5)
+        {
+           Debug.Log($"Day {dayOfTheWeek} does not belongs to weekend");        
+        }   //otherwise, if the number of the day is 6 or 7, it belongs
+        else if(dayOfTheWeek == 6 || dayOfTheWeek == 7)  
+        {   
+            Debug.Log($"Day {dayOfTheWeek} belongs to weekend");      
+        }
     }
 }
