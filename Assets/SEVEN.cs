d4e0b94e9435810782b5c38b90d3a458a36ef631
MASEPA12 <114388814+MASEPA12@users.noreply.github.com>
2022-11-07 20:17:24 +0100

    Exercise_7_done

--- Assets/SEVEN.cs
+++ Assets/SEVEN.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class SEVEN : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // VARIABLE DECLARATION
+    public string letter;
+
     void Start()
-    {
-        
+    {   
+        if(letter == "a" || letter == "e" || letter == "i" || letter == "o" || letter == "u")
+        {   //if the letter is some of the vowels, it is going to execut this:
+            Debug.Log($"The {letter} is a vowel.");
+        }
+        else //if it's not, it's going to execut this:
+        {
+            Debug.Log($"The {letter} is a consonant.");
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
