749dfdf75beeee97101da07c3bb51824e54b91df
MASEPA12 <114388814+MASEPA12@users.noreply.github.com>
2022-11-07 21:18:18 +0100

    Exercise_9_done

--- Assets/NINE.cs
+++ Assets/NINE.cs
@@ -3,16 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class NINE : MonoBehaviour
-{
-    // Start is called before the first frame update
+
+{   //VARIABLE DECLARATION
+    public int numOfCandy;
+    public int numOfPeople;
+    private int candiesPerPerson;
+    private int candiesLeft;
+
     void Start()
     {
-        
-    }
+        candiesPerPerson = numOfCandy / numOfPeople; //Division to achieve candies per person
+        candiesLeft = numOfCandy % numOfPeople; //Module to achieve candies left
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Debug.Log($"If we have {numOfCandy} candies and {numOfPeople} persons, each person should have {candiesPerPerson} candies and there are left {candiesLeft} candies.");
     }
 }
