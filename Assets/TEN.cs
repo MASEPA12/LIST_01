e4d8a91aa89e8668e8076f43e24c794e42574c93
MASEPA12 <114388814+MASEPA12@users.noreply.github.com>
2022-11-08 11:35:04 +0100

    Exercise_10_done

--- Assets/TEN.cs
+++ Assets/TEN.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class TEN : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //VARIABLE DECLARATION
+    public int tableOf = 2;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Start()
+    {   //print the predeterminate multiplications with the operation in {} 
+        Debug.Log($"{tableOf} x 1 = {tableOf}.");
+        Debug.Log($"{tableOf} x 2 = {tableOf * 2}. ");
+        Debug.Log($"{tableOf} x 3 = {tableOf * 3}. ");
+        Debug.Log($"{tableOf} x 4 = {tableOf * 4}. ");
+        Debug.Log($"{tableOf} x 5 = {tableOf * 5}. ");
+        Debug.Log($"{tableOf} x 6 = {tableOf * 6}. ");
+        Debug.Log($"{tableOf} x 7 = {tableOf * 7}. ");
+        Debug.Log($"{tableOf} x 8 = {tableOf * 8}. ");
+        Debug.Log($"{tableOf} x 9 = {tableOf * 9}. ");
+        Debug.Log($"{tableOf} x 10 = {tableOf * 10}. ");
     }
 }
